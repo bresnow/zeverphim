d92de4fe58019d1141bb33b017477b27a8868d67
bresnow <sudo@bresnow.design>
2021-08-15 16:22:40 -0400

    yoyo

--- utils/bash-scripts/nfsvol.sh
+++ utils/bash-scripts/nfsvol.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
+echo "Name your nfs volume"
+read -r NAME
 
-ssh root@3gcf.s.time4vps.cloud 'mkdir -p /store/${1}'
+mkvol "$NAME" 
+
+ssh root@3gcf.s.time4vps.cloud "mkdir -p /store/$NAME"
 
 echo "Connecting to node0"..
-ssh -p 2546 bresnow@vverkan00den.bresnow.design 'docker volume create --driver local --name "$1" --opt type=nfs --opt device=:/store/"$1" --opt o=addr=3gcf.s.time4vps.cloud,rw,nolock'
+ssh -p 2546 bresnow@vverkan00den.bresnow.design "docker volume create --driver local --name "$NAME" --opt type=nfs --opt device=:/store/"$NAME" --opt o=addr=3gcf.s.time4vps.cloud,rw,nolock"
 
 
 
 echo "Connecting to node1"..
-ssh -p 4669 bresnow@vverkan01den.bresnow.design  'docker volume create --driver local --name "$1" --opt type=nfs --opt device=:/store/"$1" --opt o=addr=3gcf.s.time4vps.cloud,rw,nolock'
+ssh -p 4669 bresnow@vverkan01den.bresnow.design  "docker volume create --driver local --name "$NAME" --opt type=nfs --opt device=:/store/"$NAME" --opt o=addr=3gcf.s.time4vps.cloud,rw,nolock"
 
 
 
 echo "Connecting to node2"..
-ssh -p 4669 bresnow@vverkan02den.bresnow.design 'docker volume create --driver local --name "$1" --opt type=nfs --opt device=:/store/"$1" --opt o=addr=3gcf.s.time4vps.cloud,rw,nolock'
+ssh -p 4669 bresnow@vverkan02den.bresnow.design "docker volume create --driver local --name "$NAME" --opt type=nfs --opt device=:/store/"$NAME" --opt o=addr=3gcf.s.time4vps.cloud,rw,nolock"
 
 
 
 echo "Connecting to node3"..
-ssh -p 4669 bresnow@vverkan03den.bresnow.design  'docker volume create --driver local --name "$1" --opt type=nfs --opt device=:/store/"$1" --opt o=addr=3gcf.s.time4vps.cloud,rw,nolock'
+ssh -p 4669 bresnow@vverkan03den.bresnow.design  "docker volume create --driver local --name "$NAME" --opt type=nfs --opt device=:/store/"$NAME" --opt o=addr=3gcf.s.time4vps.cloud,rw,nolock"'
 
-
-echo "Name your nfs volume"
-read -r name
-
-mkvol "$name" 
